33aff98fe762b03fb048defa8eb793f0c7ca4735
“H-haonan” <937605022@qq.com>
2023-08-17 15:53:47 +0800

    Site updated: 2023-08-17 15:53:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/08/01/ipxe加tftp网络引导方案/","2023/08/01/pxe服务器搭建/","2023/06/29/将镜像打包传入其他服务器/","2023/08/03/格式测试/","2023/06/29/结构测试/","2023/06/29/虚拟机部署/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/08/01/ipxe加tftp网络引导方案/","2023/08/01/pxe服务器搭建/","2023/06/29/将镜像打包传入其他服务器/","2023/06/29/结构测试/","2023/08/03/格式测试/","2023/06/29/虚拟机部署/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
